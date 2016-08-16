c5fddc6558b7c4cdc3a5f10619d36008e141ee38
Wei Zhang <natezhang@outlook.com>
2016-08-16 22:58:24 +0900

    implement hash table (chaining)

---
+++ src/main/java/com/nate/algo/ds/HashDict.java
@@ -0,0 +1,103 @@
+package com.nate.algo.ds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashDict<K, V> implements IDictionary<K, V> {
+
+    private Object[] table;  // use unchecked casting
+    private int tableSize;
+    private int size;
+
+    public HashDict() {
+        tableSize = DEFAULT_TABLE_SIZE;
+        initTable();
+    }
+
+    public HashDict(int tableSize) {
+        this.tableSize = tableSize;
+        initTable();
+    }
+
+    private void initTable() {
+        table = new Object[tableSize];
+        size = 0;
+
+        for (int i = 0; i < table.length; i++) {
+            table[i] = new ArrayList<Entry<K, V>>();
+        }
+    }
+
+    public void clear() {
+        initTable();
+    }
+
+    public V get(K key) {
+        Entry<K, V> entry = getEntry(key);
+        return (entry == null) ? null : entry.value;
+    }
+
+    public void put(K key, V value) {
+        Entry<K, V> entry = getEntry(key);
+        if (entry != null) {
+            // update entry
+            entry.value = value;
+        } else {
+            // add new entry
+            List<Entry<K, V>> entryList = getEntryList(key);
+            entryList.add(new Entry<K, V>(key, value));
+            size++;
+            assert (size >= 0);
+        }
+    }
+
+    private Entry<K, V> getEntry(K key) {
+        List<Entry<K, V>> entryList = getEntryList(key);
+        for (Entry<K, V> entry : entryList) {
+            if (entry.key.equals(key)) {
+                return entry;
+            }
+        }
+
+        return null;
+    }
+
+    private List<Entry<K, V>> getEntryList(K key) {
+        int hash = key.hashCode() % tableSize;
+        hash = (hash >= 0) ? hash : -hash;   // make hash positive
+        List<Entry<K, V>> entryList = (List<Entry<K, V>>) table[hash];
+        return entryList;
+    }
+
+    public void remove(K key) {
+        List<Entry<K, V>> entryList = getEntryList(key);
+        for (int i = 0; i < entryList.size(); i++) {
+            if (entryList.get(i).key.equals(key)) {
+                entryList.remove(i);
+                size--;
+                assert (size >= 0);
+                break;
+            }
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean empty() {
+        return size != 0;
+    }
+
+    private class Entry<K, V> {
+        K key;
+        V value;
+
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private static final int DEFAULT_TABLE_SIZE = 100;
+}
---
+++ src/test/java/com/nate/algo/ds/HashDictTest.java
@@ -0,0 +1,60 @@
+package com.nate.algo.ds;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HashDictTest {
+
+    private IDictionary<String, Integer> dict;
+
+    @Before
+    public void populate() {
+        dict = new HashDict<String, Integer>();
+        dict.put("Peter", 44);
+        dict.put("Razor", 53);
+        dict.put("Sean", 12);
+        dict.put("Jack", 33);
+    }
+
+    @After
+    public void tearDown() {
+        dict = null;
+    }
+
+    @Test
+    public void getTest() {
+        assertEquals(53, dict.get("Razor").intValue());
+        assertEquals(33, dict.get("Jack").intValue());
+        assertEquals(44, dict.get("Peter").intValue());
+        assertEquals(12, dict.get("Sean").intValue());
+        assertEquals(null, dict.get("Whatever"));
+    }
+
+    @Test
+    public void clearTest() {
+        dict.clear();
+        assertEquals(0, dict.size());
+    }
+
+    @Test
+    public void removeTest() {
+        dict.remove("Peter");
+        assertEquals(null, dict.get("Peter"));
+    }
+
+    @Test
+    public void putTest() {
+        dict.put("Shally", 33);
+        assertEquals(33, dict.get("Shally").intValue());
+    }
+
+    @Test
+    public void putAgainTest() {
+        dict.put("Peter", 53);
+        assertEquals(53, dict.get("Peter").intValue());
+    }
+
+}
